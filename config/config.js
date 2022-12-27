53dc9960366d500ecd5fd1ce24fbdaa7bbba0279
airwalk741 <airwalk741@airwalk741s-Mac-mini.local>
2022-12-27 17:19:12 +0900

    config import

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 import fs from "fs";
-
+import path from "path";
 // const serverPath = process.execPath.split("/");
 // serverPath.pop();
 // const configFileName = serverPath.join("/") + "/config/config.json";
 // let rawdata = fs.readFileSync(configFileName);
 // const config = JSON.parse(rawdata);
 
-import config from "config/config.json";
+const config = require(path.join(process.cwd(), "/config/config.json"));
 
 export { config };
