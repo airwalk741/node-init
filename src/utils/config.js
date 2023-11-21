f7409d8afbb9d22e5e0ce00948e07bf5f415adbd
airwalk741 <airwalk741@naver.com>
2023-11-21 09:16:42 +0900

    return config

--- src/utils/config.js
+++ src/utils/config.js
@@ -22,4 +22,21 @@ if (process.env.NODE_ENV !== "develop") {
 let rawdata = fs.readFileSync(configFileName);
 config = JSON.parse(rawdata);
 
+export const returnConfigData = () => {
+  let config;
+  let configFileName;
+
+  if (process.env.NODE_ENV !== "develop") {
+    const serverPath = process.execPath.split("/");
+    serverPath.pop();
+    configFileName = serverPath.join("/") + "/config/config.json";
+  } else {
+    configFileName = path.join(process.cwd(), "/config/config.json");
+  }
+
+  let rawdata = fs.readFileSync(configFileName);
+  config = JSON.parse(rawdata);
+  return config;
+};
+
 export { config };
